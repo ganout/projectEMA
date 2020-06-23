64b9e6ec4ac06ef02a24eaa9fef8c70e60a2d298
ganout <morgan.bourdillel@gmail.com>
2020-06-23 10:14:50 +0200

    init

---
+++ src/main/java/com/wildcodeschool/projectEMA/ProjectEmaApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.projectEMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEmaApplication.class, args);
+	}
+
+}
